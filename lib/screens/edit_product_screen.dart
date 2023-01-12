47953d4a2e5b02e3366f1f81b66f0b7bec0b01f7
unknown <manojbk488@gmail.com>
2023-01-12 16:22:22 +0545

    added validation to all text input fields

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -42,11 +42,24 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
   void _updateImageUrl() {
     if (!_imageUrlFocusNode.hasFocus) {
+      if((_imageUrlController.text.trim().isNotEmpty) || 
+      (!_imageUrlController.text.startsWith('http') 
+      && !_imageUrlController.text.startsWith('https'))|| 
+      (!_imageUrlController.text.endsWith('.png') 
+      && !_imageUrlController.text.endsWith('.jpg') 
+      && !_imageUrlController.text.endsWith('.jpeg'))){
+          return;
+        }                   
+                      
       setState(() {});
     }
   }
 
   void _saveForm() {
+    final isValid = _form.currentState!.validate();
+    if(!isValid){
+      return;
+    }
     _form.currentState!.save();
     print(_editedProduct.title);
     print(_editedProduct.price);
@@ -77,6 +90,12 @@ class _EditProductScreenState extends State<EditProductScreen> {
                 decoration: InputDecoration(
                   labelText: 'Title',
                 ),
+                validator: (value){
+                  if(value!.trim().isEmpty){
+                    return 'Please enter a title';
+                  }
+                  return null;
+                },
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
                 },
@@ -97,6 +116,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   labelText: 'Price',
                 ),
                 focusNode: _priceFocusNode,
+                validator: (value){
+                  if(value!.trim().isEmpty){
+                    return 'Please enter a price';
+                  }
+                  if(double.tryParse(value) == null){
+                    return 'Please enter a valid number';
+                  }
+                  if(double.parse(value) <= 0){
+                    return 'Please enter a number greater than zero';
+                  }
+                  return null;
+                },
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_descriptionFocusNode);
                 },
@@ -117,6 +148,15 @@ class _EditProductScreenState extends State<EditProductScreen> {
                   labelText: 'Description',
                 ),
                 focusNode: _descriptionFocusNode,
+                validator: (value){
+                  if(value!.trim().isEmpty){
+                    return 'Please enter a description';
+                  }
+                  if(value.length < 10){
+                    return 'Should be at least 10 characters';
+                  }
+                  return null;
+                },
                 onSaved: (value) {
                   _editedProduct = Product(
                     id: '',
@@ -161,6 +201,21 @@ class _EditProductScreenState extends State<EditProductScreen> {
                       ),
                       focusNode: _imageUrlFocusNode,
                       onFieldSubmitted: (_) => _saveForm(),
+                      validator: (value) {
+                        if (value!.trim().isEmpty) {
+                          return 'Please enter an image URL.';
+                        }
+                        if (!value.startsWith('http') &&
+                            !value.startsWith('https')) {
+                          return 'Please enter a valid URL.';
+                        }
+                        if (!value.endsWith('.png') &&
+                            !value.endsWith('.jpg') &&
+                            !value.endsWith('.jpeg')) {
+                          return 'Please enter a valid image URL.';
+                        }
+                        return null;
+                      },
                       onSaved: (value) {
                         _editedProduct = Product(
                           id: '',
